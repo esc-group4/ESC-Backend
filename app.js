edbcdd66e15b282128a5583156e8ba9da0cc3c82
Asyraf Omar <asyrafomar68@gmail.com>
2024-07-21 07:05:56 +0800

    added firebase verification

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
 import express from 'express';
 import { getAllStaff, getStaff, insertStaff} from './models/staff.js';
 import { connectToDatabase, cleanup } from './models/db.js'; // Assuming db connection functions are in db.js
+import cors from "cors"; // Add this to the list of imports
+import dotenv from "dotenv"; // Add to import list
+import { verifyToken } from './middleware/verifyToken.js';
+
 
 const app = express();
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 // Middleware to connect to the database on startup
-app.use(async (req, res, next) => {
+/* app.use(async (req, res, next) => {
   try {
     await connectToDatabase();
     next();
   } catch (err) {
     console.error('Database connection error: ', err);
     res.status(500).send('Database connection error');
+  }
+}); */
+
+app.get("/", (req, res) => {
+  res.send("working fine");
+});
+
+
+const dummyUserData = {
+  id: 1,
+  firebase_uid: "abc123",
+  name: "John Doe",
+  email: "john@example.com",
+  role: "admin"
+};
+
+
+app.post('/api/verifyToken', verifyToken, async (req, res) => {
+  const uid = req.user.uid;
+
+  try {
+
+    /* const [rows] = await db.execute('SELECT * FROM users WHERE firebase_uid = ?', [uid]);
+    
+    if (rows.length === 0) {
+      return res.status(404).send('User not found');
+    }
+
+    const user = rows[0];
+    res.json(user); */
+    console.log(`Received UID: ${uid}`);
+    res.json(dummyUserData);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ message: "Internal Error" });
   }
 });
 
@@ -63,7 +107,7 @@ process.on('SIGINT', async () => {
   process.exit(0);
 });
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ middleware/verifyToken.js
@@ -0,0 +1,21 @@
+import auth from "../config/firebase-config.js";
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+      const decodeValue = await auth.verifyIdToken(token);
+      if (decodeValue) {
+        req.user = decodeValue;
+        return next();
+      } else {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (e) {
+      return res.status(500).json({ message: "Internal Error" });
+    }
+  };
