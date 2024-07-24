b81d73b2c904d651e8ee40f2b92766afe91ae868
Do Viet Anh <119051232+vanhthefirst@users.noreply.github.com>
2024-07-24 22:04:39 +0800

    Update MySQL.session.sql

--- MySQL.session.sql
+++ MySQL.session.sql
@@ -16,26 +16,26 @@ CREATE TABLE Designation (
 	super VARCHAR(100) NOT NULL,
 	department_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY (position),
-    FOREIGN KEY (department_name) REFERENCES Department(department_name) ON DELETE CASCADE,
+    	FOREIGN KEY (department_name) REFERENCES Department(department_name) ON DELETE CASCADE,
 	UNIQUE(position)
 );
 
 -- Create Staff table references from the Designation
 CREATE TABLE Staff (
-    staff_id INT NOT NULL AUTO_INCREMENT,
-    staff_first_name VARCHAR(100) NOT NULL,
+	staff_id INT NOT NULL AUTO_INCREMENT,
+	staff_first_name VARCHAR(100) NOT NULL,
 	staff_last_name VARCHAR(100) NOT NULL,
 	staff_email VARCHAR(100) NOT NULL,
-    position VARCHAR(100) NOT NULL,
-    training_id INT,
+	position VARCHAR(100) NOT NULL,
+	training_id INT,
 	PRIMARY KEY (staff_id),
-    FOREIGN KEY (position) REFERENCES Designation(position) ON DELETE CASCADE,
+	FOREIGN KEY (position) REFERENCES Designation(position) ON DELETE CASCADE,
 	FOREIGN KEY (training_id) REFERENCES Training(training_id),
 	UNIQUE (staff_id)
 );
 
 CREATE TABLE Skill (
-    skill_name VARCHAR(100) NOT NULL,
+    	skill_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY (skill_name),
 	UNIQUE(skill_name)
 );
@@ -53,16 +53,16 @@ CREATE TABLE Course (
 
 -- CoursePlan is a child table of Training table
 CREATE TABLE Training (
-    training_id INT IDENTITY(600,1) NOT NULL, --not unique so multiple staffs attend a training
-    grade CHAR(1),
-    startDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+	training_id INT IDENTITY(600,1) NOT NULL, --not unique so multiple staffs attend a training
+	grade CHAR(1),
+	startDate DATETIME DEFAULT CURRENT_TIMESTAMP,
 	endDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    completedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    staff_id INT, -- able to have no staff in training
-    course_id INT, -- able to have no course in training because of no staff
+	completedDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+	staff_id INT, -- able to have no staff in training
+	course_id INT, -- able to have no course in training because of no staff
 	PRIMARY KEY (training_id),
-    FOREIGN KEY (staff_id) REFERENCES Staff(id) ON DELETE CASCADE, 
-    FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE CASCADE
+	FOREIGN KEY (staff_id) REFERENCES Staff(id) ON DELETE CASCADE, 
+	FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE CASCADE
 );
 
 CREATE TABLE TrainingRequest (
@@ -82,32 +82,32 @@ CREATE TABLE TrainingRequest (
 -- insert values
 INSERT INTO dbo.Designation (department_name, department_location)
 VALUES ('CNC Machine Operator', 'Busan'),
-			('Tooling Operator', 'Tokyo'),
-			('Chip Cleaner', 'Taipei'),
-			('Tool Crib Leader', 'Kuala Lumpur'),
-			('Maintenance/Construction', 'Shenzhen'),
-			('Machinist', 'Guangzhou'),
-			('Conventional Grinding', 'Pohang'),
-			('Saw cut', 'Yokohama'),
-			('Slim3N Leader/Machinist', 'Kaohsiung');
+	('Tooling Operator', 'Tokyo'),
+	('Chip Cleaner', 'Taipei'),
+	('Tool Crib Leader', 'Kuala Lumpur'),
+	('Maintenance/Construction', 'Shenzhen'),
+	('Machinist', 'Guangzhou'),
+	('Conventional Grinding', 'Pohang'),
+	('Saw cut', 'Yokohama'),
+	('Slim3N Leader/Machinist', 'Kaohsiung');
 
 
 INSERT INTO dbo.Course (course_name)
 VALUES ('Material Planner'),
-			('Operator'),
-			('CNC Machinst'),
-			('Packing');
+	('Operator'),
+	('CNC Machinst'),
+	('Packing');
 
 INSERT INTO dbo.Staff(staff_first_name, staff_last_name, staff_email, position, training_id)
 VALUES ('Javier', 'Tan', 'javiertan@TSH.com', 202, 609),
-			 ('Vivian', 'Quek', 'QuekVivian@TSH.com', 208, 604),
-			 ('Jun Jie', 'Wang', 'Junjie20@TSH.com', 209, 603),
-			 ('Hwee Ru', 'Ong', 'OnghweeRu20@TSH.com', 202, 602),
-			 ('Tommy', 'Lim', 'tomlim@TSH.com', 205, 601);
+	 ('Vivian', 'Quek', 'QuekVivian@TSH.com', 208, 604),
+	 ('Jun Jie', 'Wang', 'Junjie20@TSH.com', 209, 603),
+	 ('Hwee Ru', 'Ong', 'OnghweeRu20@TSH.com', 202, 602),
+	 ('Tommy', 'Lim', 'tomlim@TSH.com', 205, 601);
 
 
 INSERT INTO Training (grade, startDate, endDate, staff_id, course_id)
 VALUES ('A', '2024-05-21', '2024-07-21', 104, 500),
-			 ('A', '2024-02-03', '2024-03-09', 103, 503),
-			 ('A', '2024-02-05', '2024-10-07', 102, 503),			 
-			 ('B', '2024-07-29', '2023-07-11', 101, 501);
+	 ('A', '2024-02-03', '2024-03-09', 103, 503),
+	 ('A', '2024-02-05', '2024-10-07', 102, 503),			 
+	 ('B', '2024-07-29', '2023-07-11', 101, 501);
