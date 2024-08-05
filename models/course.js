2c698bb15e6f7e144a60d04c3ae66b51e721105d
benny <bennyrobot1143@gmail.com>
2024-08-05 15:38:00 +0800

    add course byStaffId

--- models/course.js
+++ models/course.js
@@ -35,4 +35,53 @@ async function all() {
     }
 }
 
-export { table, all };
+class TrainingCourse {
+    constructor(obj) {
+        const columns = [
+            "grade",
+            "attendance",
+            "type",
+            "reasons",
+            "completedDateTime",
+            "startDate",
+            "endDate",
+            "course_name",
+            "providerName",
+            "skill_name",
+            "course_location",
+            "course_description"
+        ].forEach(name => this[name] = obj[name]);
+        this.status = this.computeStatus();
+    }
+
+    computeStatus() {
+        const current_date = new Date(new Date().toDateString());
+        if (current_date <= this.startDate) return 'Upcoming';
+        if (this.attendance == 1) return 'Completed';
+        if (current_date > this.endDate && this.attendance == 0) return 'Expired';
+        return 'Invalid Status';
+    }
+}
+
+async function byStaffId(id) {
+    try {
+        const [rows, fieldDefs] = await pool.query(`
+            SELECT grade, attendance, type, reasons, completedDateTime, startDate, endDate, TrainingRequest.course_name, providerName, skill_name, course_location, course_description FROM Training
+            LEFT JOIN TrainingRequest
+            ON TrainingRequest.request_id = Training.request_id
+            LEFT JOIN Course
+            ON TrainingRequest.course_name = Course.course_name
+            WHERE staff_id = ?
+        `, [id]);
+        return rows.map(row => {
+            const tc = new TrainingCourse(row);
+            console.log(tc.status);
+            return tc;
+        });
+    } catch (error) {
+        console.error(`Failed to get all ${tableName}s` + error);
+        throw error;
+    }
+}
+
+export { table, all, byStaffId };
--- routes/course.js
+++ routes/course.js
@@ -1,10 +1,20 @@
-import { all } from '../models/course.js';
+import { all, byStaffId } from '../models/course.js';
 import express from 'express';
 var router = express.Router();
 
 router.get('/all', async (req, res) => {
     try {
         res.json(await all());
+    } catch (err) {
+        console.error('Error retrieving course: ', err);
+        res.status(500).send('Error retrieving course');
+    }
+});
+
+router.get('/staff/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        res.json(await byStaffId(id));
     } catch (err) {
         console.error('Error retrieving course: ', err);
         res.status(500).send('Error retrieving course');
